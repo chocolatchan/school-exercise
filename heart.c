02eb8dc8c43abc36fd4afd902564bd00113d20de
chocolatchan <kimizukn@gmail.com>
2024-10-23 20:14:07 +0700

    Update heart.c

--- heart.c
+++ heart.c
@@ -2,13 +2,14 @@
 #include <math.h>
 
 int f(int x, int n) {
-	return (int)round(-sqrt(n * n - pow(round(2 * x) - n, 2)) + n);
+	return (int) round(-sqrt(n * n - pow(round(2 * x) - n, 2)) + n);
 }
 int g(int x, int n) {
-	return (int)-abs(2 * x - n / 2) + n / 2;
+	return (int) -fabs(2 * x - n / 2) + n / 2 - 1;
 }
 
-void F(int n, char c) {
+void F(int n, char* c) {
+	int pnt = 0;
 	for (int i = 0; i <= n / 5; ++i) {
 		int arr[3] = { 0 };
 		arr[0] = f(i, n / 4);
@@ -19,22 +20,23 @@ void F(int n, char c) {
 			if ((j > arr[0]) && (j < arr[0] + arr[1] + 1)
 				|| (j > arr[2]) && (j < arr[2] + arr[1] + 1)
 				|| (j >= arr[0] + arr[1] + 1 && j <= arr[2]) && (i > n / 8)) {
-				printf("%c", c);
+				printf("%c", *c);
 			}
 			else {
 				printf(" ");
 			}
+			pnt = arr[0];
 		}
 		printf("\n");
 	}
-	for (int i = n / 24; i <= n / 4; ++i) {
+	for (int i = (pnt / 2) + 2; i <= n / 4; ++i) {
 		int arr[2] = { 0 };
 		arr[0] = g(i, n);
 		arr[1] = n - arr[0];
 		/*printf("i: %d\t[%d %d]\t|", i, arr[0], arr[1]);*/
 		for (int j = 0; j <= n; ++j) {
 			if (j < arr[1] + 1 && j > arr[0]) {
-				printf("%c", c);
+				printf("%c", *c);
 			} else {
 				printf(" ");
 			}
@@ -44,8 +46,10 @@ void F(int n, char c) {
 }
 
 int main() {
-	int n = 100;
-	char c = '=';
+	int n = 0;
+	printf("Enter any character and heart size (no space between size and character): ");
+	scanf("%d", &n);
+	char c = getchar();
 	/*printf("Enter heart size and heart character: ");*/
-	F(n, c);
+	F(n, &c);
 }
